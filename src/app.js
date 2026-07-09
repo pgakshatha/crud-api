7b48998bcc7a705d27518a99ef5f054607376c18
AkshathaPG <akshathapg2000@gmail.com>
2026-07-09 09:52:59 +0530

    Configure PostgreSQL connection

--- src/app.js
+++ src/app.js
@@ -6,28 +6,29 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "CRUD API is running"
-    });
+  res.json({
+    success: true,
+    message: "CRUD API is running",
+  });
 });
 
 app.get("/health", async (req, res) => {
-    try {
-        await pool.query("SELECT NOW()");
+  try {
+    await pool.query("SELECT NOW()");
 
-        res.status(200).json({
-            status: "UP",
-            application: "Running",
-            database: "Connected"
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "DOWN",
-            application: "Running",
-            database: "Disconnected",
-            error: error.message
-        });
-    }
+    res.status(200).json({
+      status: "UP",
+      application: "Running",
+      database: "Connected",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "DOWN",
+      application: "Running",
+      database: "Disconnected",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
